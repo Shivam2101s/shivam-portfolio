72d2d41cc9a10b97fddb03056e3ea30f2af4d8c4
Shivam Sharma <mechbuddy.shivam@gmail.com>
2022-01-07 17:21:29 +0530

    commit

---
+++ src/portfolio.js
@@ -0,0 +1,172 @@
+//I Would Highly Recommend You To Customize The Theme According To Your Taste.
+export const globalStyles = {
+  "--grad": "linear-gradient(315deg,#48a9fe,#00cdac)", // Use gradients only
+  // "--grad": "linear-gradient(315deg, #48a9fe 0%, #734ae8 74%)", // Use gradients only
+  // "--grad": "linear-gradient(315deg, #06bcfb 0%, #4884ee 74%)",
+  "--txtcolor": "#272341",
+  "--p_color": "#00cdac",
+  "--s_color": "#fff",
+};
+
+// Navigation Bar Name
+export const navbar_name = "<Shivam>";
+
+// Main Page
+export const main = {
+  name: "Shivam Sharma",
+  doamin: "A MERN Stack Web Developer",
+  resume:
+    "https://drive.google.com/file/d/1ZFKJeDeuMwhMUY6vi53GFnMrE_wwfofS/view?usp=sharing",
+};
+
+// AboutMe
+export const aboutme = {
+  imgLink:
+    "https://github.com/Shivam2101s/images/blob/main/profile.png?raw=true",
+  p1: "Hey, My name is Shivam Shivam. I have completed my Bachelor of Engineering from Lakshmi Narain College of Technology, Indore(M.P.).",
+  p2: " I'm a passionate coder and an enthusiastic Web Developer capable of applying programming skills to solve complex problems. Seeking a challenging role in the field of software development that will offer an opportunity for further development of my abilities, skills and knowledge in an organization.",
+  p3: "Apart from coding I also like photography 📸, cooking 👨‍🍳 and riding 🏍️."
+ 
+};
+
+// Skills
+export const whatido = {
+  title: "Full Stack Development",
+
+  details: [
+    "Building responsive website using HTML,CSS,JS,React",
+    "Creating backend application in Node, ExpressJS",
+    "Building Simple RESTful CRUD APIs",
+    "Knowledge of MongoDB Database",
+    "Knowledge of various UI libraries",
+  ],
+  logos: [
+    {
+      name: "HTML5",
+      iconifyClassName: "vscode-icons:file-type-html", // PS : Use iconify class name
+      id: "1",
+    },
+    {
+      name: "CSS",
+      iconifyClassName: "vscode-icons:file-type-css",
+      id: "2",
+    },
+    {
+      name: "Material-UI",
+      iconifyClassName: "logos:material-ui",
+      id: "9",
+    },
+    {
+      name: "Tailwind",
+      iconifyClassName: "logos:tailwindcss-icon",
+      id: "10",
+    },
+    {
+      name: "JavaScript",
+      iconifyClassName: "logos:javascript",
+      id: "3",
+    },
+    {
+      name: "ReactJS",
+      iconifyClassName: "vscode-icons:file-type-reactjs",
+      id: "4",
+    },
+    {
+      name: "NodeJS",
+      iconifyClassName: "logos:nodejs",
+      id: "5",
+    },
+    {
+      name: "ExpressJS",
+      iconifyClassName: "logos:express",
+      id: "13",
+    },
+    {
+      name: "MongoDB",
+      iconifyClassName: "logos:mongodb",
+      id: "6",
+    },
+    {
+      name: "Postman",
+      iconifyClassName: "logos:postman-icon",
+      id: "12",
+    },
+
+    {
+      name: "VS Code",
+      iconifyClassName: "vscode-icons:file-type-vscode",
+      id: "14",
+    },
+    {
+      name: "GIT",
+      iconifyClassName: "logos:git",
+      id: "15",
+    },
+  ],
+};
+
+// Project Details
+export const details = [
+  {
+    id: 1,
+    title: "Frontend Masters Clone",
+    desc: `Frontend Masters is an E-learning platform where users can advance their skills with in-depth, modern front-end engineering courses.
+Features:
+Sign-in/Sign-up,
+Explore top courses and teachers,
+Dynamic progress monitor page,
+Subscription page |
+Tech Stack: HTML | CSS | JavaScript | EJS | nodeJS | express | MongoDB`,
+    img: "https://github.com/Shivam2101s/images/blob/main/frontend.jpg?raw=true",
+    
+    github: "https://github.com/bhargavkatira/front-end-master",
+  },
+  {
+    id: 2,
+    title: "Reliance Retail Clone",
+    desc: `Reliance Retail is an informative website where users can get information about various brands of Reliance Industries.
+Features:
+Sign-in/Sign-up, User Authentication,
+Navbar,
+Carousel,
+Add to cart & Check out |
+Tech Stack: HTML | CSS | JavaScript .`,
+    img: "https://github.com/Shivam2101s/images/blob/main/reliance(1)%20-%20Copy.jpg?raw=true",
+    github: "https://github.com/Shivam2101s/Reliance-retails",
+    link: "https://relianceretailclone.netlify.app/",
+  },
+  {
+    id: 3,
+    title: "Eva Translate",
+    desc: `Eva Translate is a translating tool, users can translate text from one language to another and also hear its correct pronunciation.
+    Features:
+    Language translation,
+    Speech-to-text |
+    Tech Stack: HTML | CSS | JavaScript .`,
+    img: "https://github.com/Shivam2101s/images/blob/main/eva%20translate.jpg?raw=true",
+    link: "https://evatranslate.netlify.app/",
+    github: "https://github.com/Shivam2101s/Eva-Translate",
+  },
+  {
+    id: 4,
+    title: "My Recipe App",
+    desc: `It's a food recipe app, users can search recipes of various dishes and also see the latest recipes as well as the recipe of the day.
+Features:
+Search recipe,
+Recipe of the day | 
+Tech Stack: HTML | CSS | JavaScript .`,
+    img: "https://github.com/Shivam2101s/images/blob/main/food%20app.jpg?raw=true",
+    link: "https://myfoodapp21.netlify.app/",
+    github: "https://github.com/Shivam2101s/Food-app",
+  },
+];
+
+// Social Media Links
+export const links = [
+  "https://www.instagram.com/s_sharma.ji/",
+  "https://www.linkedin.com/in/shivam2101s/",
+  "https://api.whatsapp.com/send/?phone=918269795767&text&app_absent=0",
+  "mailto:shivam.2101s@gmail.com",
+  "https://shivam2101s.medium.com/",
+  "https://github.com/Shivam2101s",
+];
